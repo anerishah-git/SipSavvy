08d034b780d94aa7bd3979739728b901f7669305
anerishah-git <112180072+anerishah-git@users.noreply.github.com>
2023-08-15 01:40:38 -0400

    14/8

---
+++ js.js
@@ -0,0 +1,7 @@
+function openAbt() {
+    document.getElementById("abt").style.right = "0";
+}
+/* place the navigation off the page */
+function closeAbt() {
+    document.getElementById("abt").style.right = "-200rem";
+}
