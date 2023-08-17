591368ba5ee7559d4322f5998abc771ec5cfbcc5
anerishah-git <112180072+anerishah-git@users.noreply.github.com>
2023-08-17 03:34:24 -0400

    17/8

--- js.js
+++ js.js
@@ -1,7 +1,7 @@
 function openAbt() {
-    document.getElementById("abt").style.right = "0";
+    document.getElementById("abt").style.top = "0";
 }
 /* place the navigation off the page */
 function closeAbt() {
-    document.getElementById("abt").style.right = "-200rem";
+    document.getElementById("abt").style.top = "-200rem";
 }
