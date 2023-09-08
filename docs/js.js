ef952db146b3215bb553e4845d1cca7ceb629775
anerishah-git <112180072+anerishah-git@users.noreply.github.com>
2023-09-08 00:31:54 -0400

    push files

---
+++ docs/js.js
@@ -0,0 +1,7 @@
+function openAbt() {
+    document.getElementById("abt").style.top = "0";
+}
+/* place the navigation off the page */
+function closeAbt() {
+    document.getElementById("abt").style.top = "-200rem";
+}
